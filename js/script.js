a1603bff59d1678416816978030fb5fb6890fcfb
shlnmzq <147102174+skylann1@users.noreply.github.com>
2024-02-11 10:05:21 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// toggle class aktive
+const navBarNav = document.querySelector('.navbar-nav');
+// // ketika humbergur menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navBarNav.classList.toggle('active');
+// };
+
+
+// klick di luar sidebar 
+const hamburger = document.querySelector('#hamburger-menu')
+
+document.addEventListener('click', function (e) {
+    if (!hamburger.contains(e.target) && !navBarNav.contains(e.target)) {
+       navBarNav.classList.remove('active');
+  }
+});
+}
+
